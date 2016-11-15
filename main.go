8f2f0d27bc6652117bab77c725ac6e2e44332550
Alexander Ekdahl <alexander@ekdahl.io>
2016-11-15 17:24:13 +0900

    Changeable port

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	log.Printf("Server started at http://localhost:8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("You need to set PORT environement variable")
+	}
+
+	log.Printf("Server started at http://localhost:%v", port)
 	router := NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), router))
 }
